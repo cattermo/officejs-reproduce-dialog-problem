28150e8fbb9e365fd263deba9267b32cf1e06302
Clara Attermo <clara.attermo@scrive.com>
2024-05-13 08:31:08 +0200

    Try set "Cross-Origin-Opener-Policy": "unsafe-none"

--- index.js
+++ index.js
@@ -5,12 +5,17 @@ const app = express();
 
 app.set("view engine", "ejs");
 
-app.use("/public", express.static(path.join(process.cwd(), "public")));
+app.use("/public", express.static(path.join(process.cwd(), "public"), {
+  setHeaders: (res) => {
+    res.setHeader("Cross-Origin-Opener-Policy", "unsafe-none");
+  }
+}));
 
 app.set("view engine", "hbs");
 app.set("views", [path.join(process.cwd(), "views"), path.join(process.cwd(), "public")]);
 
 app.use("/api/login",(req, res) => {
+  res.setHeader("Cross-Origin-Opener-Policy", "unsafe-none");
   res.render("login", {url: "https://google.com"})
 });
 
